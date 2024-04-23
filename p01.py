8785c6cafce5e38c19821c899ed3a6e4259c02df
Thmn93 <163934575+Thmn93@users.noreply.github.com>
2024-04-22 22:00:27 -0300

    Add files via upload

---
+++ p01.py
@@ -0,0 +1,6 @@
+numero1 = int(input('Insira o primeiro número:'))
+numero2 = int(input('Insira o segundo número:'))
+numero3 = int(input('Insira o terceiro número:'))
+lista = reversed=[numero1,numero2,numero3]
+decrescente = sorted(lista)
+print (lista)
---
+++ p02.py
@@ -0,0 +1,11 @@
+pais1 = 80000
+pais2 = 200000
+anual1 = int(pais1*3/100)+pais1
+anual2 = int(pais2*1.5/100)+pais2
+while (
+      anual2 > anual1):
+    print(in range)
+    if anual1 > anual2:
+        break
+final = (anual1 > anual2)
+print(final)
